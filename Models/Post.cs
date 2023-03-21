989f13ad1bc6ece31bc725acb302fa3d34261eed
Rodrigo Moncorvo <romoncorvo@gmail.com>
2023-03-21 13:02:31 +0100

    Add upload

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,19 +1,21 @@
-// using Azure;
-// using Azure.Data.Tables;
-//
-// namespace SaltGram.API.Models;
-//
-// public class Post :ITableEntity
-// {
-//     public string PartitionKey { get; set; }
-//     public string RowKey { get; set; }
-//     public DateTimeOffset? Timestamp { get; set; }
-//     public ETag ETag { get; set; }
-//
-//     public int UserId { get; set; }
-//     public virtual User? User { get; set; }
-//     public string PictureUrl { get; set; }
-//     public string Description { get; set; }
-//     public string[] Comments { get; set; }
-//     public int Likes { get; set; }
-// }
+using Azure;
+using Azure.Data.Tables;
+
+namespace SaltGram.API.Models;
+
+public class Post :ITableEntity
+{
+    public string PartitionKey { get; set; }
+    public string RowKey { get; set; }
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+
+    public int UserId { get; set; }
+    // public virtual User? User { get; set; }
+    public string PictureUrl { get; set; }
+    public string Description { get; set; }
+    public string[] Comments { get; set; }
+    public int Likes { get; set; }
+}
+
+
---
+++ Services/ImageStorageService.cs
@@ -0,0 +1,38 @@
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace SaltGram.API.Services;
+
+public class ImageStorageService
+{
+    public ImageStorageService()
+    {
+    }
+
+    public async Task<string> UploadFile()
+    {
+        // Retrieve the connection string for use with the application. 
+        string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+        
+
+        // Create a BlobServiceClient object 
+        var blobServiceClient = new BlobServiceClient(connectionString);
+        var containerClient = blobServiceClient.GetBlobContainerClient("images");
+        
+
+        string localPath = "./data/";
+        string localFilePath = Path.Combine(localPath, "aPicture.gif");
+
+        // Upload
+        string fileName = "aPicture.gif";
+        var blobClient = containerClient.GetBlobClient(fileName);
+        
+        var blobHttpHeader = new BlobHttpHeaders();
+
+        blobHttpHeader.ContentType = "image/gif";
+
+        await blobClient.UploadAsync(localFilePath, blobHttpHeader);
+        
+        return blobClient.Uri.ToString();
+    }
+}
